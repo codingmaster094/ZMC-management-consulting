001141c93dbe686858be25fe8b0c86b50de09fb8
codingmaster094 <codingmaster094@gmail.com>
2025-08-07 18:07:24 +0530

    add indexing

--- app/robots.txt/route.js
+++ app/robots.txt/route.js
@@ -1,8 +1,7 @@
 export async function GET() {
   const body = `User-agent: *
-Disallow:
-
-Sitemap: https://zmc-management-consulting.vercel.app/sitemap.xml`;
+                Allow: /
+                Sitemap: https://zmc-management-consulting.vercel.app/sitemap.xml`;
 
   return new Response(body, {
     headers: {
